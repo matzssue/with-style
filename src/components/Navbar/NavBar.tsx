24c87a7c2c39bada3a5c9f059c32bbae0bab6745
Mateusz Kluska <mateuszkluska7@gmail.com>
2024-05-18 13:24:36 +0200

    feat: add button to toggle navbar ( temporary)

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -24,64 +24,71 @@ import {
   shoesNavLinks,
 } from '@/constants/navlist';
 import { NavigationList } from '../NavigationList/NavigationList';
-// legacy behacior passhref do sprawdzenia
+import { useSidebarStore } from '@/store/useSidebarStore';
 
 export function NavBar() {
-  return (
-    <NavigationMenu className='hidden lg:flex '>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <Link href='/home'>
-            <NavigationMenuLink
-              style={{ fontSize: '24px' }}
-              className={navigationMenuTriggerStyle()}
-            >
-              Home
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger style={{ fontSize: '24px' }}>
-            Clothing
-          </NavigationMenuTrigger>
-          <NavigationMenuContent className='z-50'>
-            <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]'>
-              <NavigationList list={clothingNavLinks} />
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger style={{ fontSize: '24px' }}>
-            Shoes
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]'>
-              <NavigationList list={shoesNavLinks} />
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger style={{ fontSize: '24px' }}>
-            Accessories
-          </NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]'>
-              <NavigationList list={accessoriesNavLinks} />
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
+  const toggleSidebar = useSidebarStore((state) => state.toggleNavigation);
 
-        <NavigationMenuItem>
-          <Link href='/about-us'>
-            <NavigationMenuLink
-              style={{ fontSize: '24px' }}
-              className={navigationMenuTriggerStyle()}
-            >
-              About Us
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+  return (
+    <>
+      <button className='lg:hidden' onClick={() => toggleSidebar()}>
+        XXXXXXX
+      </button>
+      <NavigationMenu className='hidden lg:flex '>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href='/home' legacyBehavior passHref>
+              <NavigationMenuLink
+                style={{ fontSize: '24px' }}
+                className={navigationMenuTriggerStyle()}
+              >
+                Home
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger style={{ fontSize: '24px' }}>
+              Clothing
+            </NavigationMenuTrigger>
+            <NavigationMenuContent className='z-50'>
+              <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]'>
+                <NavigationList list={clothingNavLinks} />
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger style={{ fontSize: '24px' }}>
+              Shoes
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]'>
+                <NavigationList list={shoesNavLinks} />
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger style={{ fontSize: '24px' }}>
+              Accessories
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]'>
+                <NavigationList list={accessoriesNavLinks} />
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <Link href='/about-us' legacyBehavior passHref>
+              <NavigationMenuLink
+                style={{ fontSize: '24px' }}
+                className={navigationMenuTriggerStyle()}
+              >
+                About Us
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </>
   );
 }
