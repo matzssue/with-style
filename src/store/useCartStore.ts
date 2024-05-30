2cc5f057e6f74de6ac8a95c63d739c3ae55c470e
Mateusz Kluska <mateuszkluska7@gmail.com>
2024-05-30 10:42:37 +0200

    feat: change type for for actions, product in store

--- src/store/useCartStore.ts
+++ src/store/useCartStore.ts
@@ -1,12 +1,19 @@
 import { create } from 'zustand'
-import type { Product } from '@prisma/client'
-import { persist, createJSONStorage } from 'zustand/middleware'
-import { it } from 'node:test'
-type ProductQuantity = {
+import type { ProductCategory, ProductType, Size } from '@prisma/client'
+import { persist } from 'zustand/middleware'
+
+export type ProductInStore = {
+  id: string
+  storeId: string
+  name: string
+  price: number
+  category: ProductCategory
+  subcategory: string | null
+  type: ProductType
+  imgUrl: string
+  size: Size | null
   quantity: number
 }
-
-export type ProductInStore = Product & ProductQuantity
 
 type Store = {
   cart: ProductInStore[]
@@ -15,9 +22,9 @@ type Store = {
 }
 
 type Actions = {
-  addToCart: (Item: Product) => void
-  removeFromCart: (Item: Product) => void
-  decreaseQuantity: (Item: Product) => void
+  addToCart: (Item: ProductInStore) => void
+  removeFromCart: (Item: ProductInStore) => void
+  decreaseQuantity: (Item: ProductInStore) => void
 }
 
 const INITIAL_STATE = {
@@ -32,17 +39,12 @@ export const useCartStore = create(
       cart: INITIAL_STATE.cart,
       totalItems: INITIAL_STATE.totalItems,
       totalPrice: INITIAL_STATE.totalPrice,
-      addToCart: (product: Product) => {
+      addToCart: (product: ProductInStore) => {
         const cart = get().cart
-        const cartItem = cart.find((item) => item.id === product.id)
-
+        const cartItem = cart.find((item) => item.storeId === product.storeId)
         if (cartItem) {
           const updatedCart = cart.map((item) => {
-            if (!item.quantity) {
-              item.quantity = 1
-            }
-
-            return item.id === product.id
+            return item.storeId === product.storeId
               ? { ...item, quantity: item.quantity + 1 }
               : item
           })
@@ -60,19 +62,18 @@ export const useCartStore = create(
           }))
         }
       },
-      decreaseQuantity: (product: Product) => {
+      decreaseQuantity: (product: ProductInStore) => {
         const cart = get().cart
-        const cartItem = cart.find((item) => item.id === product.id)
-        console.log('my cart item', cartItem)
-        if (cartItem?.id === product.id && cartItem?.quantity > 1) {
+        const cartItem = cart.find((item) => item.storeId === product.storeId)
+        if (cartItem && cartItem?.quantity > 1) {
           const updatedCart = cart.map((item) => {
-            if (item.id === cartItem.id) {
+            if (item.storeId === cartItem.storeId) {
               return { ...item, quantity: item.quantity - 1 }
             } else {
               return item
             }
           })
-          console.log(updatedCart, 'updated cart')
+
           set((state) => ({
             cart: updatedCart,
             totalItems: state.totalItems - 1,
@@ -80,15 +81,15 @@ export const useCartStore = create(
           }))
         } else {
           set((state) => ({
-            cart: state.cart.filter((item) => item.id !== product.id),
+            cart: state.cart.filter((item) => item.storeId !== product.storeId),
             totalItems: state.totalItems - 1,
             totalPrice: +(state.totalPrice - product.price).toFixed(2),
           }))
         }
       },
-      removeFromCart: (product: Product) => {
+      removeFromCart: (product: ProductInStore) => {
         set((state) => ({
-          cart: state.cart.filter((item) => item.id !== product.id),
+          cart: state.cart.filter((item) => item.storeId !== product.storeId),
           totalItems: 0,
           totalPrice: +(state.totalPrice - product.price).toFixed(2),
         }))
