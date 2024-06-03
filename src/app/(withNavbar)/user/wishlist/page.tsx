98fcf1704e7a6d34f973f840c7f6f214b7675731
Mateusz Kluska <mateuszkluska7@gmail.com>
2024-06-03 23:01:44 +0200

    feat: add page with components for wishlist items

---
+++ src/app/(withNavbar)/user/wishlist/page.tsx
@@ -0,0 +1,9 @@
+import { WishlistMenu } from './(components)/WishlistMenu'
+
+export default function Wishlist() {
+  return (
+    <section className='mx-[10%]'>
+      <WishlistMenu />
+    </section>
+  )
+}
