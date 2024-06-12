275b0b386c6eaac947083e94ac394ef9f3739d0f
Mateusz Kluska <mateuszkluska7@gmail.com>
2024-06-12 19:46:45 +0200

    feat: add title to wishlist

--- src/app/(withNavbar)/user/wishlist/page.tsx
+++ src/app/(withNavbar)/user/wishlist/page.tsx
@@ -3,6 +3,7 @@ import { WishlistMenu } from './(components)/WishlistMenu'
 export default function Wishlist() {
   return (
     <section className='mx-[10%]'>
+      <h1 className='py-5 text-center text-3xl'>You'r wishlist</h1>
       <WishlistMenu />
     </section>
   )
