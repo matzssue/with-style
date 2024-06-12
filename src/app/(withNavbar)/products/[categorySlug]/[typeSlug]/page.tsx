1b4c2666cbec35b1ac176bbfb0bdd4adea4087fb
Mateusz Kluska <mateuszkluska7@gmail.com>
2024-06-12 19:51:25 +0200

    feat: change axios to default fetch

--- src/app/(withNavbar)/products/[categorySlug]/[typeSlug]/page.tsx
+++ src/app/(withNavbar)/products/[categorySlug]/[typeSlug]/page.tsx
@@ -3,11 +3,19 @@ import { ProductList } from '@/app/(withNavbar)/(components)/ProductsList'
 import { ProductsMenu } from '@/app/(withNavbar)/(components)/ProductsMenu'
 import { ReadonlyURLSearchParams } from 'next/navigation'
 import { Params } from 'next/dist/shared/lib/router/utils/route-matcher'
+import { getWishlistProductsId } from '@/actions/get-wishlist'
+import { auth } from '@/auth'
 
 type ProductSearchParams = {
   [key: string]: string
 }
-
+type QueryParams = {
+  category?: string
+  type?: string
+  size?: string
+  minPrice?: string
+  maxPrice?: string
+}
 export default async function TypeProducts({
   params,
   searchParams,
@@ -15,29 +23,42 @@ export default async function TypeProducts({
   params: Params
   searchParams: ProductSearchParams
 }) {
-  async function fetchProducts() {
-    const response = await axios.get(
-      `http://localhost:3000/api/products/${params.categorySlug}/${params.typeSlug}`,
-      {
-        params: {
-          category: params.categorySlug,
-          type: params.typeSlug,
-          minPrice: searchParams.minPrice,
-          maxPrice: searchParams.maxPrice,
-        },
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    )
+  const session = await auth()
+  const userId = session?.user.id
 
-    if (response.status !== 200) {
+  const getProducts = async () => {
+    const url = new URL(
+      `http://localhost:3000/api/products/${params.categorySlug}/${params.typeSlug}`
+    )
+    console
+    const queryParams: QueryParams = {}
+    if (params.categorySlug) queryParams.category = params.categorySlug
+    if (params.typeSlug) queryParams.type = params.typeSlug
+    if (searchParams.minPrice) queryParams.minPrice = searchParams.minPrice
+    if (searchParams.maxPrice) queryParams.maxPrice = searchParams.maxPrice
+    if (searchParams.size) queryParams.size = searchParams.size
+    url.search = new URLSearchParams(queryParams).toString()
+
+    const response = await fetch(url.toString(), {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      next: { tags: ['typeProducts'] },
+    })
+
+    if (!response.ok) {
       throw new Error('Failed to fetch products')
     }
 
-    return response.data
+    const data = await response.json()
+    return data
   }
+  console.log('session', session)
 
-  const products = await fetchProducts()
-  return <ProductList products={products} />
+  const products = await getProducts()
+  const userWishlist = await getWishlistProductsId(userId)
+  const wishlist = session ? userWishlist : []
+
+  return <ProductList wishlisted={wishlist} products={products} />
 }
