4d94fc3848c52f5fe55876459e7b3919dad7d6a1
Mateusz Kluska <mateuszkluska7@gmail.com>
2024-06-17 21:19:10 +0200

    feat: add total pages to returned data

--- src/app/api/products/[categorySlug]/route.ts
+++ src/app/api/products/[categorySlug]/route.ts
@@ -1,18 +1,37 @@
 import { NextRequest, NextResponse } from 'next/server'
 import prisma from '@/lib/prisma'
 import { ProductCategory, Size } from '@prisma/client'
+import { ITEMS_PER_PAGE } from '@/constants/pages'
+import { ProductsData } from '@/types/products'
 export async function GET(request: NextRequest) {
   try {
     const searchParams = request.nextUrl.searchParams
     const categories = searchParams.get('category')
     const size = searchParams.get('size') as Size
-
+    const page = searchParams.get('page')
     const minPrice = searchParams.get('minPrice') as string
     const maxPrice = searchParams.get('maxPrice') as string
 
     const categoryToUpper = categories?.toUpperCase() as ProductCategory
+    const pageNumber = Number(page)
+    const skip = (pageNumber - 1) * ITEMS_PER_PAGE
 
     const products = await prisma.product.findMany({
+      take: ITEMS_PER_PAGE,
+      skip: skip,
+      where: {
+        category: categoryToUpper,
+        ...(size !== null && { size: { has: size } }),
+        ...(maxPrice &&
+          minPrice !== null && {
+            price: {
+              gte: +minPrice,
+              lte: +maxPrice,
+            },
+          }),
+      },
+    })
+    const count = await prisma.product.count({
       where: {
         category: categoryToUpper,
         ...(size !== null && { size: { has: size } }),
@@ -26,7 +45,16 @@ export async function GET(request: NextRequest) {
       },
     })
 
-    return NextResponse.json(products)
+    const totalPages = Math.ceil(count / ITEMS_PER_PAGE)
+
+    const productsData: ProductsData = {
+      data: products,
+      metadata: {
+        totalPages,
+      },
+    }
+
+    return NextResponse.json(productsData)
   } catch (error) {
     console.log(error)
     return NextResponse.error()
