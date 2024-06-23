0e851fc117e409eb15e7300f4b9001af780f6f86
Mateusz Kluska <mateuszkluska7@gmail.com>
2024-06-23 13:32:20 +0200

    feat: add routes form reset and new password

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,11 @@
 export const publicRoutes = ['/', '/cart', '/auth/new-verification']
-export const authRoutes = ['/auth/login', '/auth/register', '/auth/error', ,]
+export const authRoutes = [
+  '/auth/login',
+  '/auth/register',
+  '/auth/error',
+  '/auth/reset',
+  '/auth/new-password',
+]
 export const privateRoutes = ['/user/wishlist']
 export const dynamicRoutes = [
   '/products',
