761fcfe3141debcb5179619fca3784617ca6e062
Mateusz Kluska <mateuszkluska7@gmail.com>
2024-06-24 19:53:26 +0200

    feat: move button link  (login page) to register form

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,17 +1,11 @@
+import RegisterForm from '@/components/Auth/RegisterForm'
 import Link from 'next/link'
-import RegisterForm from '../../../components/Forms/RegisterForm'
 
 export default async function RegisterPage() {
   return (
     <>
       <h1 className='py-5 text-center text-2xl font-bold'>Register account</h1>
       <RegisterForm />
-      <p className='text-xl'>
-        Arleady registered?
-        <Link className='ml-3 font-bold' href='login'>
-          Sign In
-        </Link>
-      </p>
     </>
   )
 }
