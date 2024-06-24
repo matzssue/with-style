85f90fdf4392fb5f822e9e88ba8dd7779b67cb7b
Mateusz Kluska <mateuszkluska7@gmail.com>
2024-06-24 19:47:04 +0200

    feat: add settings schema

--- src/lib/schemas/auth-schema.ts
+++ src/lib/schemas/auth-schema.ts
@@ -1,10 +1,11 @@
+import { newPassword } from '@/actions/new-password'
 import { z } from 'zod'
 
 export type RegisterSchema = z.infer<typeof registerSchema>
 export type LoginSchema = z.infer<typeof loginSchema>
 export type EmailSchema = z.infer<typeof emailSchema>
 export type PasswordSchema = z.infer<typeof passwordSchema>
-
+export type SettingsSchema = z.infer<typeof settingsSchema>
 export const registerSchema = z
   .object({
     username: z.string().min(4, {
@@ -48,3 +49,38 @@ export const passwordSchema = z.object({
     message: 'Minimum of 6 characters required',
   }),
 })
+export const settingsSchema = z
+  .object({
+    name: z.optional(z.string()),
+    email: z.optional(z.string().email()),
+    password: z.optional(z.string().min(6)),
+    newPassword: z.optional(z.string().min(6)),
+  })
+  .refine(
+    (data) => {
+      if (data.password && !data.newPassword) {
+        return false
+      }
+      if (data.newPassword && !data.password) {
+        return false
+      }
+      return true
+    },
+    {
+      message: 'New password is required!',
+      path: ['newPassword'],
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.newPassword && !data.password) {
+        return false
+      }
+
+      return true
+    },
+    {
+      message: 'Password is required!',
+      path: ['password'],
+    }
+  )
