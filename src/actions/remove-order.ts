9feb60c40ef528e9f8d04191c4f8bca96302d4e8
Mateusz Kluska <mateuszkluska7@gmail.com>
2024-07-03 22:44:22 +0200

    feat: add action to remove order

---
+++ src/actions/remove-order.ts
@@ -0,0 +1,25 @@
+'use server'
+
+export const removeOrder = async (orderId: string) => {
+  try {
+    const response = await fetch('http://localhost:3000/api/order/delete', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        orderId,
+      }),
+    })
+
+    const data = await response.json()
+
+    return data
+  } catch (error) {
+    let message = 'Unknown Error'
+    if (error instanceof Error) {
+      message = error.message
+    }
+    return { error: message }
+  }
+}
