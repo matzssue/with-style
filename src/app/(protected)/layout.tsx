b12acca99a230f8d7114aaa99bccb1649f0b2e3a
Mateusz Kluska <mateuszkluska7@gmail.com>
2024-07-09 21:31:38 +0200

    feat: add session to session provider

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,16 +1,18 @@
+import { auth } from '@/auth'
 import Header from '@/components/Header/Header'
 import { NavBar } from '@/components/Navbar/NavBar'
 import UserBar from '@/components/UserBar/UserBar'
 import Wrapper from '@/components/Wrapper/Wrapper'
 import { SessionProvider } from 'next-auth/react'
 
-export default function ProtectedLayout({
+export default async function ProtectedLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const session = await auth()
   return (
-    <SessionProvider>
+    <SessionProvider session={session}>
       <Header staticNavigation={<NavBar />} />
       <UserBar />
       <Wrapper>{children}</Wrapper>
