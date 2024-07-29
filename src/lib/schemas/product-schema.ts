9ff8a9f317b45c55dde2ff4ab96b42c1c7c25ce2
Mateusz Kluska <mateuszkluska7@gmail.com>
2024-07-29 13:48:49 +0200

    feat: add schema for add product

--- src/lib/schemas/product-schema.ts
+++ src/lib/schemas/product-schema.ts
@@ -1,10 +1,12 @@
-import { ProductCategory, ProductType } from '@prisma/client'
+import { ProductCategory, ProductType, Size } from '@prisma/client'
 import { z } from 'zod'
 
 export type ProductSchema = z.infer<typeof productSchema>
+export type AddProductSchema = z.infer<typeof addProductSchema>
 
 const categories = z.nativeEnum(ProductCategory)
 const types = z.nativeEnum(ProductType)
+const sizes = z.nativeEnum(Size)
 
 export const productSchema = z.object({
   id: z.string(),
@@ -14,4 +16,14 @@ export const productSchema = z.object({
   subcategory: z.string().min(3).max(20).nullable(),
   imgUrl: z.string().min(3).max(200),
   type: types,
+})
+
+export const addProductSchema = z.object({
+  name: z.string().min(3).max(15),
+  price: z.number().positive('Price must be a positive number'),
+  category: categories,
+  subcategory: z.string().nullable(),
+  imgUrl: z.string().url('Invalid URL format'),
+  type: types,
+  size: z.array(sizes).optional(),
 })
