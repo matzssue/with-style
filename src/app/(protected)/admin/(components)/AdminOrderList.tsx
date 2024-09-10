ee231b82405c7548152248ef585ad5481841b56e
matzssue <mateuszkluska7@gmail.com>
2024-09-10 22:48:31 +0200

    feat: move orders list to separate component

---
+++ src/app/(protected)/admin/(components)/AdminOrderList.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { OrdersList } from '@/app/(withNavbar)/user/orders/(components)/OrdersList'
+import { SearchInput } from '@/components/Searchbar/SearchInput'
+import { Input } from '@/components/ui/input'
+import { useSelectList } from '@/hooks/use-select-list'
+import { OrderData } from '@/types/orders'
+import { ChangeEvent } from 'react'
+
+export const AdminOrderList = ({ orders }: { orders: OrderData[] }) => {
+  const filterByCategory = (item: OrderData, searchValue: string) => {
+    return item.orderNumber
+      .toString()
+      .toLowerCase()
+      .includes(searchValue.toLowerCase())
+  }
+
+  const { listData, searchValueHandler, searchValue } =
+    useSelectList<OrderData>(orders, filterByCategory)
+
+  return (
+    <>
+      <div>
+        <SearchInput
+          placeholder='search by order number'
+          title='Search order'
+          setSearchValue={searchValueHandler}
+          value={searchValue}
+        />
+      </div>
+      <OrdersList ordersData={listData} />
+    </>
+  )
+}
--- src/app/(protected)/admin/orders/page.tsx
+++ src/app/(protected)/admin/orders/page.tsx
@@ -1,18 +1,8 @@
-'use client'
-
-import { ProductList } from '@/app/(withNavbar)/products/(components)/ProductsList'
 import { Paginator } from '@/components/Paginator/Paginator'
-import { getProducts } from '@/data/products/get-products'
-import { AdminProductList } from '../(components)/AdminProductList'
-import { OrdersList } from '@/app/(withNavbar)/user/orders/(components)/OrdersList'
-import { getOrders } from '@/data/orders/get-orders'
+
 import { getAllOrders } from '@/data/orders/get-all-orders'
-import { useSelectList } from '@/hooks/use-select-list'
-import { Order } from '@prisma/client'
-import { OrderData, OrdersData } from '@/types/orders'
-import { ChangeEvent } from 'react'
-import { Input } from '@/components/ui/input'
-// import { useEffect, useState } from 'react'
+
+import { AdminOrderList } from '../(components)/AdminOrderList'
 
 type OrdersSearchParams = {
   page: string
@@ -25,32 +15,10 @@ export default async function OrdersAdmin({
 }) {
   const pageNumber = Number(searchParams.page || 1)
   const { data: orders, metadata } = await getAllOrders(pageNumber)
-  console.log('orders', orders)
-  const filterByCategory = (item: OrderData, searchValue: string) => {
-    return item.orderNumber
-      .toString()
-      .toLowerCase()
-      .includes(searchValue.toLowerCase())
-  }
-  if (!orders) return
-  const { listData, searchValueHandler, searchValue } =
-    useSelectList<OrderData>(orders, filterByCategory)
 
   return (
     <div>
-      <div>
-        <label>Search order</label>
-        <Input
-          type='search'
-          value={searchValue ?? ''}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            searchValueHandler(e.target.value)
-          }
-          id='searchInput'
-          placeholder='search '
-        />
-      </div>
-      <OrdersList ordersData={listData} />
+      <AdminOrderList orders={orders} />
       <Paginator page={pageNumber} totalPages={metadata.totalPages} />
     </div>
   )
