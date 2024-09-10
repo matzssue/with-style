dfe0d7defa059b384f6699ee121c55514919ff8f
matzssue <mateuszkluska7@gmail.com>
2024-09-10 22:58:14 +0200

    feat: add try/catch, change headers

--- src/data/orders/get-order.ts
+++ src/data/orders/get-order.ts
@@ -1,33 +1,37 @@
-'use server'
-
 import { OrderData } from '@/types/orders'
-
+import { headers } from 'next/headers'
 type QueryParams = {
   orderId?: string
 }
 
 export async function getOrder(orderId: string) {
-  const url = new URL(
-    `${process.env.NEXT_PUBLIC_VERCEL_DOMAIN}/api/order-details/${orderId}`
-  )
-  const queryParams: QueryParams = {}
+  try {
+    const url = new URL(
+      `${process.env.NEXT_PUBLIC_VERCEL_DOMAIN}/api/order-details/${orderId}`
+    )
+    const queryParams: QueryParams = {}
 
-  if (orderId) queryParams.orderId = orderId
+    if (orderId) queryParams.orderId = orderId
 
-  url.search = new URLSearchParams(queryParams).toString()
+    url.search = new URLSearchParams(queryParams).toString()
 
-  const response = await fetch(url.toString(), {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    cache: 'no-cache',
-  })
+    const response = await fetch(url.toString(), {
+      method: 'GET',
+      headers: headers(),
+      cache: 'no-cache',
+    })
 
-  if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status}`)
+    if (!response.ok) {
+      throw new Error(`Failed to fetch order. Status: ${response.status}`)
+    }
+
+    const data: OrderData = await response.json()
+
+    return data
+  } catch (err) {
+    if (err instanceof Error) {
+      throw new Error(err.message)
+    }
+    throw new Error('Something went wrong')
   }
-
-  const data: OrderData = await response.json()
-  return data
 }
