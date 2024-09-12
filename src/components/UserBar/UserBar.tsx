7d91898cb61894273a11f897f41cdda9d561b790
matzssue <mateuszkluska7@gmail.com>
2024-09-12 14:04:09 +0200

    feat: remove searchbar

--- src/components/UserBar/UserBar.tsx
+++ src/components/UserBar/UserBar.tsx
@@ -8,9 +8,9 @@ import { currentUser } from '@/lib/auth/auth'
 export default async function UserBar() {
   const user = await currentUser()
   return (
-    <div className='flex flex-row justify-between bg-neutral-100 p-5 max-md:flex-col  max-md:gap-y-5'>
-      <Searchbar />
-      <div className='flex items-center justify-between gap-8 align-middle max-md:justify-center '>
+    // <div className='flex w-full max-w-[150px] flex-row justify-end self-end rounded-bl-[60px] bg-neutral-100 p-5  max-md:flex-col max-md:gap-y-5'>
+    <div className=' absolute right-0 top-20 max-w-[150px] flex-row justify-end self-end rounded-bl-[60px] bg-neutral-100 p-5  max-md:flex-col max-md:gap-y-5'>
+      <div className='flex justify-end gap-8  align-middle max-md:justify-center '>
         <ShoppingBag />
         {user ? (
           <UserMenu user={user} />
