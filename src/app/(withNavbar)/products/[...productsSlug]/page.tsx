f2f087c00a4f4df33d7646d4430a8cf8d712d120
matzssue <mateuszkluska7@gmail.com>
2024-09-13 16:26:18 +0200

    feat: add sorting functionality

--- src/app/(withNavbar)/products/[...productsSlug]/page.tsx
+++ src/app/(withNavbar)/products/[...productsSlug]/page.tsx
@@ -7,12 +7,14 @@ import { ProductsData, ProductsQueryParams } from '@/types/products'
 import { Paginator } from '@/components/Paginator/Paginator'
 import { ProductList } from '../(components)/ProductsList'
 import { getProducts } from '@/data/products/get-products'
+import { SortingMenu } from '../(components)/SortingMenu'
 
 type ProductSearchParams = {
   minPrice?: string
   maxPrice?: string
   size?: string
   page?: string
+  sortByPrice?: string
 }
 
 export default async function TypeProducts({
@@ -25,7 +27,6 @@ export default async function TypeProducts({
   const session = await auth()
   const userId = session?.user.id
   const pageNumber = Number(searchParams.page || 1)
-
   const categorySlug = params.productsSlug[0]
   const typeSlug = params.productsSlug[1]
 
@@ -35,6 +36,8 @@ export default async function TypeProducts({
   if (searchParams.minPrice) queryParams.minPrice = searchParams.minPrice
   if (searchParams.maxPrice) queryParams.maxPrice = searchParams.maxPrice
   if (searchParams.size) queryParams.size = searchParams.size
+  if (searchParams.sortByPrice)
+    queryParams.sortByPrice = searchParams.sortByPrice
   queryParams.page = pageNumber.toString()
 
   const { data, metadata }: ProductsData = await getProducts(queryParams)
@@ -44,6 +47,7 @@ export default async function TypeProducts({
 
   return (
     <div className='flex w-full flex-col'>
+      <SortingMenu />
       <ProductList wishlisted={wishlist} products={data} />
       <div className='flex w-full items-center'>
         <Paginator page={pageNumber} totalPages={metadata.totalPages} />
