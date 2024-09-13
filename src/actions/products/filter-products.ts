5bc593ec350bc20e9d5f01632cf3561295f4f640
matzssue <mateuszkluska7@gmail.com>
2024-09-13 16:28:16 +0200

    feat: add sorting to filters

--- src/actions/products/filter-products.ts
+++ src/actions/products/filter-products.ts
@@ -4,21 +4,26 @@ import { redirect } from 'next/navigation'
 
 type Filters = {
   size?: string
-  price: number[]
+  maxPrice?: string | null
+  minPrice?: string | null
+  sortByPrice?: string | null
 }
 
 type QueryParams = {
-  maxPrice: string
-  minPrice: string
+  maxPrice?: string
+  minPrice?: string
   size?: string
+  sortByPrice?: string
 }
 
 export const updateFilters = async (filters: Filters, currentPath: string) => {
-  const queryParams: QueryParams = {
-    minPrice: filters.price[0].toString(),
-    maxPrice: filters.price[1].toString(),
-  }
-  if (filters.size) queryParams.size = filters.size
+  const queryParams: QueryParams = {}
+  const { maxPrice, minPrice, size, sortByPrice } = filters
+
+  if (minPrice) queryParams.minPrice = minPrice.toString()
+  if (maxPrice) queryParams.maxPrice = maxPrice.toString()
+  if (size) queryParams.size = filters.size
+  if (sortByPrice) queryParams.sortByPrice = sortByPrice
 
   if (filters) {
     const params = new URLSearchParams(queryParams)
