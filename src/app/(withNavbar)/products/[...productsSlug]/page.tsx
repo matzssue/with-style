b632ce6277ad8a983e8bf92b4a6760ffeee43192
matzssue <mateuszkluska7@gmail.com>
2024-09-22 11:31:02 +0200

    feat: add subcategory

--- src/app/(withNavbar)/products/[...productsSlug]/page.tsx
+++ src/app/(withNavbar)/products/[...productsSlug]/page.tsx
@@ -2,21 +2,16 @@ import { Params } from 'next/dist/shared/lib/router/utils/route-matcher'
 import { getWishlistProductsId } from '@/data/wishlist/get-wishlist'
 import { auth } from '@/auth'
 
-import { ProductsData, ProductsQueryParams } from '@/types/products'
+import {
+  ProductsData,
+  ProductSearchParams,
+  ProductsQueryParams,
+} from '@/types/products'
 
 import { Paginator } from '@/components/Paginator/Paginator'
 import { ProductList } from '../(components)/ProductsList'
 import { getProducts } from '@/data/products/get-products'
 import { SortingMenu } from '../(components)/SortingMenu'
-
-type ProductSearchParams = {
-  minPrice?: string
-  maxPrice?: string
-  size?: string
-  page?: string
-  sortByPrice?: string
-  promotions?: boolean
-}
 
 export default async function TypeProducts({
   params,
@@ -30,10 +25,12 @@ export default async function TypeProducts({
   const pageNumber = Number(searchParams.page || 1)
   const categorySlug = params.productsSlug[0]
   const typeSlug = params.productsSlug[1]
-
+  console.log(categorySlug, typeSlug, 'slugs')
   const queryParams: ProductsQueryParams = {}
   if (categorySlug) queryParams.category = categorySlug
   if (typeSlug) queryParams.type = typeSlug
+  if (searchParams.subcategory)
+    queryParams.subcategory = searchParams.subcategory
   if (searchParams.minPrice) queryParams.minPrice = searchParams.minPrice
   if (searchParams.maxPrice) queryParams.maxPrice = searchParams.maxPrice
   if (searchParams.size) queryParams.size = searchParams.size
