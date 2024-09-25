1d84ee0e6bdd2070cb940b00ca202739f24982f7
matzssue <mateuszkluska7@gmail.com>
2024-09-25 19:20:35 +0200

    feat: add mapped shoe size

--- src/constants/sizes.ts
+++ src/constants/sizes.ts
@@ -2,3 +2,4 @@ export const sizes = ['S', 'M', 'L', 'XL', '2XL']
 export const shoesSize = [
   32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
 ]
+export const shoeSizeToString = shoesSize.map((size) => size.toString())
