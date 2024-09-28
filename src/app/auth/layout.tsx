297093d544b02ba92c2b93a78d5038ad441c0398
matzssue <mateuszkluska7@gmail.com>
2024-09-28 16:00:11 +0200

    style: improve responsiveness

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,11 +3,11 @@ import Logo from '@images/Logo.png'
 import Image from 'next/image'
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className='flex h-screen w-full flex-col items-center justify-center bg-secondary'>
+    <div className='flex h-screen w-full flex-col items-center justify-center bg-secondary px-2'>
       <header className=''>
         <Image alt='logo' src={Logo} />
       </header>
-      <div className='flex w-1/3 flex-col gap-y-5 rounded-md bg-neutral-100 p-10 shadow-lg'>
+      <div className='mx-5 flex w-full max-w-[500px] flex-col gap-y-5 rounded-md bg-neutral-100 p-10 shadow-lg'>
         {children}
       </div>
     </div>
