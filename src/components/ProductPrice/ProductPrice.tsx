b64b80f13b7f4bad01637af021e5221c54417914
matzssue <mateuszkluska7@gmail.com>
2024-09-28 11:17:43 +0200

    style: layout improvements

--- src/components/ProductPrice/ProductPrice.tsx
+++ src/components/ProductPrice/ProductPrice.tsx
@@ -1,5 +1,4 @@
 import { calculatePriceWithDiscount } from '@/lib/helplers/calculatePriceWithDiscount'
-import { Product } from '@prisma/client'
 
 export const ProductPrice = ({
   price,
@@ -13,11 +12,11 @@ export const ProductPrice = ({
   const discountPrice = calculatePriceWithDiscount(price, discountPercentage)
 
   return (
-    <p className={`flex flex-${direction}`}>
-      Price:{' '}
+    <div className={`flex flex-${direction} gap-1`}>
+      Price:
       <span className={`font-semibold ${discountPrice ? 'line-through' : ''}`}>
         {price}$
-      </span>{' '}
+      </span>
       {discountPercentage && (
         <div className='flex gap-2 px-1'>
           <span className='flex font-semibold text-red-500'>
@@ -28,6 +27,6 @@ export const ProductPrice = ({
           </span>
         </div>
       )}
-    </p>
+    </div>
   )
 }
