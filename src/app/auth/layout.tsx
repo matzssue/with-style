b0c21cc2a837061acc16817d748b768184761cdf
matzssue <mateuszkluska7@gmail.com>
2024-09-29 14:33:37 +0200

    style: change size of layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,11 +3,11 @@ import Logo from '@images/Logo.png'
 import Image from 'next/image'
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className='flex h-screen w-full flex-col items-center justify-center bg-secondary px-2'>
-      <header className=''>
+    <div className='flex min-h-screen w-full flex-col items-center justify-center bg-secondary px-2 py-4'>
+      <header>
         <Image alt='logo' src={Logo} />
       </header>
-      <div className='mx-5 flex w-full max-w-[500px] flex-col gap-y-5 rounded-md bg-neutral-100 p-10 shadow-lg'>
+      <div className='mx-5 flex h-full w-full max-w-[650px] flex-col gap-y-5 rounded-md bg-neutral-100 p-10 shadow-lg'>
         {children}
       </div>
     </div>
