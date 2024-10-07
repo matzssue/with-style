a75cf70473e7b0be0347b04fda22cdd02a4ed44b
matzssue <mateuszkluska7@gmail.com>
2024-10-07 21:57:59 +0200

    feat: add size types

--- src/constants/sizes.ts
+++ src/constants/sizes.ts
@@ -3,3 +3,10 @@ export const shoesSize = [
   32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
 ]
 export const shoeSizeToString = shoesSize.map((size) => size.toString())
+
+export const sizeType = {
+  MAN: ['S', 'M', 'L', 'XL', '2XL'],
+  WOMAN: ['S', 'M', 'L', 'XL', '2XL'],
+  ACCESSIORES: [],
+  SHOES: shoeSizeToString,
+}
