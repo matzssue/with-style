37ff8e3f353db1f353c4e49ad5ca8b88047140d4
matzssue <mateuszkluska7@gmail.com>
2024-10-07 21:55:24 +0200

    feat: add validation for data

--- src/actions/products/add-product.ts
+++ src/actions/products/add-product.ts
@@ -1,12 +1,22 @@
 'use server'
 
 import { productPath } from '@/constants/revalidation-keys'
+import { productSchema } from '@/lib/schemas/product-schema'
+
 import { Product } from '@prisma/client'
 import { revalidatePath } from 'next/cache'
 
 type AddProductData = Omit<Product, 'id'>
 
 export const addProduct = async (product: AddProductData) => {
+  const productData = productSchema.safeParse(product)
+
+  if (productData.error) {
+    return {
+      error: productData.error.message,
+    }
+  }
+  console.log(productData)
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_VERCEL_DOMAIN}/api/admin/product/add`,
@@ -23,6 +33,7 @@ export const addProduct = async (product: AddProductData) => {
       throw new Error(`HTTP error! Status: ${response.status}`)
     }
     revalidatePath(productPath)
+
     await response.json()
     return { success: 'Product added' }
   } catch (error) {
--- src/actions/products/edit-product.ts
+++ src/actions/products/edit-product.ts
@@ -1,10 +1,20 @@
 'use server'
 
 import { productPath } from '@/constants/revalidation-keys'
+import { productSchema } from '@/lib/schemas/product-schema'
+
 import { Product } from '@prisma/client'
 import { revalidatePath } from 'next/cache'
 
 export const editProduct = async (updatedProduct: Partial<Product>) => {
+  const productData = productSchema.safeParse(updatedProduct)
+
+  if (productData.error) {
+    return {
+      error: productData.error.message,
+    }
+  }
+
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_VERCEL_DOMAIN}/api/admin/product/edit`,
