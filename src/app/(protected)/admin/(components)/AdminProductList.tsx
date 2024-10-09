b2504bd736de8b52882efecc18b4b4a561eeac6a
matzssue <mateuszkluska7@gmail.com>
2024-10-09 19:09:13 +0200

    feat: change useSelectList  to useSearch hook
    refactor searching to work on backend instead of frontend

--- src/app/(protected)/admin/(components)/AdminProductList.tsx
+++ src/app/(protected)/admin/(components)/AdminProductList.tsx
@@ -4,39 +4,36 @@ import { ProductCard } from '@/components/Cards/ProductCard'
 import { Product } from '@prisma/client'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
-import { SearchInput } from '@/components/Searchbar/SearchInput'
-import { useSelectList } from '@/hooks/use-select-list'
+
 import { DeleteProductButton } from './DeleteProduct'
 import { adminRoutes } from '@/routes'
+import { Input } from '@/components/ui/input'
+import { useSearch } from '@/hooks/use-search'
 
 export const AdminProductList = ({ products }: { products: Product[] }) => {
-  const filterByName = (item: Product, searchValue: string) => {
-    return item.name
-      .toString()
-      .toLowerCase()
-      .includes(searchValue.toLowerCase())
-  }
-
-  const { listData, searchValueHandler, searchValue } = useSelectList<Product>(
-    products,
-    filterByName
-  )
+  const { handleSearch } = useSearch(adminRoutes.products)
 
   return (
     <div className='my-5 flex flex-col flex-wrap gap-5 px-5'>
       <div className='flex w-full gap-4'>
-        <SearchInput
-          placeholder='search by product name'
-          title='Search product'
-          setSearchValue={searchValueHandler}
-          value={searchValue}
+        <label className='text-nowrap text-2xl font-bold'>
+          Search by product name
+        </label>
+        <Input
+          type='text'
+          id='searchInput'
+          placeholder={''}
+          style={{ maxWidth: '300px' }}
+          onChange={(e) => {
+            handleSearch(e.target.value)
+          }}
         />
         <Button asChild>
           <Link href={`/${adminRoutes.products}/add`}>Add new product</Link>
         </Button>
       </div>
       <div className='flex flex-wrap gap-5 max-md:justify-center'>
-        {listData.map((product) => (
+        {products.map((product) => (
           <ProductCard key={product.id} product={product}>
             <div className='flex gap-5 '>
               <Button asChild>
