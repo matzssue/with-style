2b538748f9fe8076551a80972e459386c01d25d4
matzssue <mateuszkluska7@gmail.com>
2024-10-09 20:20:59 +0200

    feat: add constant with delivery informations

---
+++ src/constants/informations.ts
@@ -0,0 +1,19 @@
+export const deliveryInformations = [
+  {
+    title: 'Delivery Price',
+    content:
+      'Enjoy flexible and free deliveries with us! Orders above 30$ are free of charge. Shop hassle-free and get your items delivered quickly, tailored to your needs.',
+    value: 'item-1',
+  },
+  {
+    title: 'Delivery Time',
+    content: 'We guarantee delivery within 1-3 days from placing the order',
+    value: 'item-2',
+  },
+  {
+    title: 'Free return within 30 days',
+    content:
+      'Take advantage of our 30-day free return option! Shop confidently, knowing you can return your items at no additional cost',
+    value: 'item-3',
+  },
+]
