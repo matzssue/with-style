a45654aec3b38ec83e85d28199c3d268d1d1126d
matzssue <mateuszkluska7@gmail.com>
2024-10-12 18:03:13 +0200

    feat: add class props to components

--- src/components/Accordion/CustomAccordionItem.tsx
+++ src/components/Accordion/CustomAccordionItem.tsx
@@ -1,22 +1,36 @@
+import { HTMLProps, ReactNode } from 'react'
 import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from '../ui/accordion'
 
+export type CustomAccordion = {
+  title: string
+  value: string
+  content?: string
+  children?: ReactNode
+  triggerCn?: HTMLProps<HTMLElement>['className']
+  contentCn?: HTMLProps<HTMLElement>['className']
+  itemCn?: HTMLProps<HTMLElement>['className']
+}
+
 export const CustomAccordionItem = ({
   title,
   value,
   content,
-}: {
-  title: string
-  value: string
-  content: string
-}) => {
+  children,
+  triggerCn,
+  contentCn,
+  itemCn,
+}: CustomAccordion) => {
   return (
-    <AccordionItem value={value}>
-      <AccordionTrigger>{title}</AccordionTrigger>
-      <AccordionContent>{content}</AccordionContent>
+    <AccordionItem className={itemCn} value={value}>
+      <AccordionTrigger className={triggerCn}>{title}</AccordionTrigger>
+      <AccordionContent className={contentCn}>
+        {content}
+        {children}
+      </AccordionContent>
     </AccordionItem>
   )
 }
