df4ef10d910240eb9edb5b49015bbee105c79ae2
matzssue <mateuszkluska7@gmail.com>
2024-10-12 13:35:13 +0200

    feat: add loading state

--- src/app/(protected)/user/(components)/WishlistToggleButton.tsx
+++ src/app/(protected)/user/(components)/WishlistToggleButton.tsx
@@ -10,6 +10,8 @@ import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import { removeFromWishlist } from '@/actions/wishlist/remove-product-from-wishlist'
 import { toast } from 'sonner'
+import { useState } from 'react'
+import { Loading } from '@/components/Loading/Loading'
 
 export const WishlistToggleButton = ({
   productId,
@@ -23,21 +25,31 @@ export const WishlistToggleButton = ({
   const session = useSession()
   const router = useRouter()
   const userId = session.data?.user.id
+  const [loading, setLoading] = useState<boolean>(false)
 
-  const handleWishlistItem = () => {
+  const handleWishlistItem = async () => {
+    setLoading(true)
     if (session.status === 'unauthenticated') {
       router.push('/auth/login')
       return
     }
+
     if (isWishlisted && userId) {
-      removeFromWishlist(userId, productId)
+      await removeFromWishlist(productId)
+
       toast('Product removed from wishlist')
     }
     if (!isWishlisted && userId) {
-      addToWishlist(userId, productId)
+      await addToWishlist(productId)
+
       toast('Product added to wishlist')
     }
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
   }
+
+  if (loading) return <Loading />
 
   return (
     <>
