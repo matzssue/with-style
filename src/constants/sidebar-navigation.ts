d21a9d152f2cd06d708f2d9f9aecceef544fdb18
matzssue <mateuszkluska7@gmail.com>
2024-10-12 18:02:32 +0200

    refactor: change accordion to generic accordion

---
+++ src/constants/sidebar-navigation.ts
@@ -0,0 +1,35 @@
+import { CustomAccordion } from '@/components/Accordion/CustomAccordionItem'
+import { ProductCategory } from '@prisma/client'
+import {
+  accessoriesNavLinks,
+  manNavLinks,
+  shoesNavLinks,
+  womanNavLinks,
+} from './navlist'
+
+export const sidebarNavigation = [
+  {
+    title: 'Man',
+    value: 'item-1',
+    navigation: manNavLinks,
+    mainLink: 'man',
+  },
+  {
+    title: 'Woman',
+    value: 'item-2',
+    navigation: womanNavLinks,
+    mainLink: 'woman',
+  },
+  {
+    title: 'Shoes',
+    value: 'item-3',
+    navigation: shoesNavLinks,
+    mainLink: 'shoes',
+  },
+  {
+    title: 'Accessories',
+    value: 'item-4',
+    navigation: accessoriesNavLinks,
+    mainLink: 'accessories',
+  },
+]
