9e8c4d744977fb8230eb1a2f454d86ec87126b05
matzssue <mateuszkluska7@gmail.com>
2024-10-14 17:19:45 +0200

    feat: add user informations from payment session

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -1,12 +1,15 @@
 import { Button } from '@/components/ui/button'
+import { getStripeSession } from '@/data/stripe/get-stripe-session'
 import { CircleCheckBig } from 'lucide-react'
 import Link from 'next/link'
 
-export default function PaymentSuccessPage({
-  searchParams: { amount },
+export default async function PaymentSuccessPage({
+  searchParams: { session_id },
 }: {
-  searchParams: { amount: string }
+  searchParams: { session_id: string }
 }) {
+  const session = await getStripeSession(session_id)
+  const sessionData = session[0]
   return (
     <main className='flex h-screen items-center justify-center bg-neutral-100 p-10'>
       <div className='flex w-1/2 flex-col gap-5 rounded-sm bg-secondary py-8 text-center shadow-md '>
@@ -19,7 +22,12 @@ export default function PaymentSuccessPage({
         <div className='flex flex-col items-center justify-center'>
           <span className='w-1/2 rounded-lg bg-primary px-2 py-4 shadow-md'>
             <p className='text-2xl text-secondary'>Amount paid:</p>
-            <p className='text-xl text-secondary'>{amount} $</p>
+            <p className='text-xl text-secondary'>
+              {sessionData.amount_total && sessionData.amount_total / 100} $
+            </p>
+            <p className='text-xl text-primary-white'>
+              Thank you {sessionData.customer_details?.name}
+            </p>
           </span>
         </div>
         <Button asChild className='w-1/2 self-center'>
