0148724a0c3a1fdbeea4f92fdba6d69772cf67b2
matzssue <mateuszkluska7@gmail.com>
2024-10-16 22:32:33 +0200

    feat: remove userId from function props

--- src/app/(withNavbar)/product/[productId]/(components)/ProductFormButtons.tsx
+++ src/app/(withNavbar)/product/[productId]/(components)/ProductFormButtons.tsx
@@ -1,15 +1,10 @@
 import { WishlistToggleButton } from '@/app/(protected)/user/(components)/WishlistToggleButton'
 import { AddProductForm } from './AddProductForm'
 import { Product } from '@prisma/client'
-import { Suspense } from 'react'
-import { Loading } from '@/components/Loading/Loading'
-import { auth } from '@/auth'
 import { getWishlistProductsId } from '@/data/wishlist/get-wishlist'
 
 export const ProductFormButtons = async ({ product }: { product: Product }) => {
-  const session = await auth()
-  const userId = session?.user.id
-  const userWishlist = await getWishlistProductsId(userId)
+  const userWishlist = await getWishlistProductsId()
 
   return (
     <div className='flex flex-col gap-2'>
