ba464cc85da53fd6b23170ab78d097f1d53502f8
matzssue <mateuszkluska7@gmail.com>
2024-10-16 23:23:49 +0200

    feat: add variant in url for admin

--- src/data/orders/get-order.ts
+++ src/data/orders/get-order.ts
@@ -1,28 +1,32 @@
 import { getCookies } from '@/lib/auth/sessionCookies'
 import { fetchData } from '@/lib/helplers/fetchData'
-import { userRoutes } from '@/routes'
+import { adminRoutes, userRoutes } from '@/routes'
 import { OrderData } from '@/types/orders'
 
 type QueryParams = {
   orderId?: string
 }
 
-export const getOrder = async (orderId: string): Promise<OrderData> => {
+export const getOrder = async (
+  orderId: string,
+  adminOrder: boolean = false
+): Promise<OrderData> => {
   const queryParams: QueryParams = {}
 
   if (orderId) queryParams.orderId = orderId
 
-  const order = await fetchData<OrderData>(
-    `api/${userRoutes.orderDetails}/${orderId}`,
-    {
-      queryParams: queryParams,
-      headers: {
-        'Content-Type': 'application/json',
-        Cookie: getCookies(),
-      },
-      cache: 'no-cache',
-    }
-  )
+  const selectedUrl = adminOrder
+    ? `api/${adminRoutes.orderDetails}/${orderId}`
+    : `api/${userRoutes.orderDetails}/${orderId}`
+
+  const order = await fetchData<OrderData>(selectedUrl, {
+    queryParams: queryParams,
+    headers: {
+      'Content-Type': 'application/json',
+      Cookie: getCookies(),
+    },
+    cache: 'no-cache',
+  })
 
   return order
 }
