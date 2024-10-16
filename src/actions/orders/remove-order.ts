36dd13158e07f595c11580a6fbe2b0425b66e596
matzssue <mateuszkluska7@gmail.com>
2024-10-16 17:55:23 +0200

    feat: change fetch function to imported custom fetch function

--- src/actions/orders/remove-order.ts
+++ src/actions/orders/remove-order.ts
@@ -1,32 +1,26 @@
 'use server'
 
 import { orderTag } from '@/constants/revalidation-keys'
+import { fetchData } from '@/lib/helplers/fetchData'
 import { adminRoutes } from '@/routes'
+import { FetchDataResponse } from '@/types/data'
+
 import { revalidateTag } from 'next/cache'
 
 export const removeOrder = async (orderId: string) => {
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_VERCEL_DOMAIN}/api/${adminRoutes.orders}/delete`,
-      {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          orderId,
-        }),
-      }
-    )
-
-    const data = await response.json()
-    revalidateTag(orderTag)
-    return data
-  } catch (error) {
-    let message = 'Unknown Error'
-    if (error instanceof Error) {
-      message = error.message
+  const order = await fetchData<FetchDataResponse>(
+    `api/${adminRoutes.orders}/delete`,
+    {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        orderId,
+      }),
     }
-    return { error: message }
-  }
+  )
+
+  revalidateTag(orderTag)
+  return order
 }
