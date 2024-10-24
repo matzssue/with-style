383677597e803fa4cf55a0383e453e87e8bed308
matzssue <mateuszkluska7@gmail.com>
2024-10-24 13:41:09 +0200

    feat: change headers

--- src/data/products/get-subcategories.ts
+++ src/data/products/get-subcategories.ts
@@ -1,13 +1,14 @@
 import { Subcategory } from '@/constants/categories'
 import { fetchData } from '@/lib/helplers/fetchData'
 import { publicRoutes } from '@/routes'
-import { headers } from 'next/headers'
 
 export const getSubcategoryTitles = async () => {
   const subcategoryTitles = await fetchData<Subcategory[]>(
     `api/${publicRoutes.products}/subcategory/titles`,
     {
-      headers: headers(),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     }
   )
 
